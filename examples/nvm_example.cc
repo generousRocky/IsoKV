307052930a1587a9439fe11311fb1eb860c88844
Javier González <javier@javigon.com>
2015-08-12 22:01:48 +0200

    nvm: fix style in examples/

--- examples/nvm_example.cc
+++ examples/nvm_example.cc
@@ -12,121 +12,102 @@ using namespace std;
 
 const char DBPath[] = "rocksdb";
 
-int main(int argc, char **argv)
-{
-    if(argc != 4)
-    {
-	if(argc == 3 && strcmp(argv[1], "read") == 0)
-	{
-	    goto proceed;
-	}
-	else if(argc == 2 && strcmp(argv[1], "fill") == 0)
-	{
-	    goto proceed;
-	}
-
-	cout << "[write/read/fill key value]" << endl << flush;
-
-	return 0;
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    if (argc == 3 && strcmp(argv[1], "read") == 0) {
+      goto proceed;
+    } else if (argc == 2 && strcmp(argv[1], "fill") == 0) {
+      goto proceed;
     }
+
+    cout << "[write/read/fill key value]" << endl << flush;
+    return 0;
+  }
 
 proceed:
 
-    const char *key = argv[2];
-    const char *value = argv[3];
+  const char *key = argv[2];
+  const char *value = argv[3];
 
-    char *err = NULL;
-    char *returned_value;
+  char *err = NULL;
+  char *returned_value;
 
-    size_t len;
+  size_t len;
 
-    rocksdb_t *db;
+  rocksdb_t *db;
+  rocksdb_options_t *options;
+  rocksdb_writeoptions_t *writeoptions;
+  rocksdb_readoptions_t *readoptions;
 
-    rocksdb_options_t *options;
+  long cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
-    rocksdb_writeoptions_t *writeoptions;
-    rocksdb_readoptions_t *readoptions;
+  options = rocksdb_options_create();
 
-    long cpus = sysconf(_SC_NPROCESSORS_ONLN);
+  rocksdb_options_increase_parallelism(options, (int)(cpus));
+  rocksdb_options_optimize_level_style_compaction(options, 0);
+  rocksdb_options_set_create_if_missing(options, 1);
 
-    options = rocksdb_options_create();
+  db = rocksdb_open(options, DBPath, &err);
+  if(err) {
+    cout << "OPEN ERROR: " << err << endl << flush;
+    return EXIT_FAILURE;
+  }
 
-    rocksdb_options_increase_parallelism(options, (int)(cpus));
-    rocksdb_options_optimize_level_style_compaction(options, 0);
+  cout << "Database is open\n" << flush;
 
-    rocksdb_options_set_create_if_missing(options, 1);
+  if(strcmp(argv[1], "write") == 0) {
+    writeoptions = rocksdb_writeoptions_create();
 
-    db = rocksdb_open(options, DBPath, &err);
-    if(err)
-    {
-	cout << "OPEN ERROR: " << err << endl << flush;
-	return EXIT_FAILURE;
+    cout << "PUT DATA " << key << ":" << value << endl << flush;
+
+    rocksdb_put(db, writeoptions, key, strlen(key), value, strlen(value) + 1,
+                                                                          &err);
+    if(err) {
+      cout << "WRITE ERROR: " << err << endl << flush;
+      return EXIT_FAILURE;
     }
 
-    cout << "Database is open\n" << flush;
+    rocksdb_writeoptions_destroy(writeoptions);
+  } else if(strcmp(argv[1], "read") == 0) {
+    readoptions = rocksdb_readoptions_create();
 
-    if(strcmp(argv[1], "write") == 0)
-    {
-	writeoptions = rocksdb_writeoptions_create();
-
-	cout << "PUT DATA " << key << ":" << value << endl << flush;
-
-	rocksdb_put(db, writeoptions, key, strlen(key), value, strlen(value) + 1, &err);
-	if(err)
-	{
-	    cout << "WRITE ERROR: " << err << endl << flush;
-	    return EXIT_FAILURE;
-	}
-
-	rocksdb_writeoptions_destroy(writeoptions);
-    }
-    else if(strcmp(argv[1], "read") == 0)
-    {
-	readoptions = rocksdb_readoptions_create();
-
-	returned_value = rocksdb_get(db, readoptions, key, strlen(key), &len, &err);
-	if(err)
-	{
-	    cout << "GET ERROR: " << err << endl << flush;
-	    return EXIT_FAILURE;
-	}
-
-	cout << "GOT DATA " << (returned_value ? returned_value : "null") << endl << flush;
-
-	free(returned_value);
-
-	rocksdb_readoptions_destroy(readoptions);
-    }
-    else if(strcmp(argv[1], "fill") == 0)
-    {
-	writeoptions = rocksdb_writeoptions_create();
-
-	for(unsigned long i = 0; i < 1000; ++i)
-	{
-	    char k_data[20];
-	    char v_data[20];
-
-	    sprintf(k_data, "%lu", i);
-	    sprintf(v_data, "%lu", i);
-
-	    rocksdb_put(db, writeoptions, k_data, strlen(k_data), v_data, strlen(v_data) + 1, &err);
-	}
-
-	rocksdb_writeoptions_destroy(writeoptions);
+    returned_value = rocksdb_get(db, readoptions, key, strlen(key), &len, &err);
+    if(err) {
+      cout << "GET ERROR: " << err << endl << flush;
+      return EXIT_FAILURE;
     }
 
-    rocksdb_garbage_collect(db, &err);
+    cout << "GOT DATA " << (returned_value ? returned_value : "null") << endl << flush;
+    free(returned_value);
+    rocksdb_readoptions_destroy(readoptions);
+  } else if(strcmp(argv[1], "fill") == 0) {
+    writeoptions = rocksdb_writeoptions_create();
 
-    if(err)
-    {
-	cout << "GARBAGE COLLECTION FAILED " << err << endl << flush;
-	return EXIT_FAILURE;
+    for(unsigned long i = 0; i < 1000; ++i) {
+      char k_data[20];
+      char v_data[20];
+
+      sprintf(k_data, "%lu", i);
+      sprintf(v_data, "%lu", i);
+
+      rocksdb_put(db, writeoptions, k_data, strlen(k_data), v_data,
+                                                    strlen(v_data) + 1, &err);
     }
 
-    rocksdb_options_destroy(options);
-    rocksdb_close(db);
+    rocksdb_writeoptions_destroy(writeoptions);
+  }
 
-    cout << "DONE\n" << flush;
+  rocksdb_garbage_collect(db, &err);
 
-    return EXIT_SUCCESS;
+  if(err) {
+    cout << "GARBAGE COLLECTION FAILED " << err << endl << flush;
+    return EXIT_FAILURE;
+  }
+
+  rocksdb_options_destroy(options);
+  rocksdb_close(db);
+
+  cout << "DONE\n" << flush;
+
+  return EXIT_SUCCESS;
 }
