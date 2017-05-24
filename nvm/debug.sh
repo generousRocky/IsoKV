f164a09935e2c7bc9f3d93fbd05a5bd1ac902d90
Simon A. F. Lund <slund@cnexlabs.com>
2017-05-24 23:23:42 +0200

    Updating test-scripts

--- nvm/debug.sh
+++ nvm/debug.sh
@@ -6,6 +6,6 @@ popd
 rm /tmp/*.sst
 rm /tmp/rdb_*.log
 rm /opt/rocks/nvme0/*
-./scripts/run.sh scripts/env_shared.sh ./scripts/env_nvm_nvme0.sh fillseq,readrandom,readseq,readwhilewriting 0 2>&1 | tee -a /tmp/rdb_all.log
+./scripts/run.sh scripts/env_shared.sh ./scripts/env_nvm_p8.sh fillseq,readrandom,readseq,readwhilewriting 0 2>&1 | tee -a /tmp/rdb_all.log
 #./scripts/run.sh scripts/env_shared.sh ./scripts/env_nvm_nvme0.sh fillseq 0 2>&1 | tee -a /tmp/rdb_fillseq.log
 #./scripts/run.sh scripts/env_shared.sh ./scripts/env_nvm_nvme0.sh readseq 1 2>&1 | tee -a /tmp/rdb_readseq.log
