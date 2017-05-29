afc417ce01674710cebfdb809361caa8fd5d9eb5
Simon A. F. Lund <slund@cnexlabs.com>
2017-05-29 22:13:47 +0200

    scripts

--- nvm/debug.sh
+++ nvm/debug.sh
@@ -5,6 +5,7 @@ if [ $UID != 0 ]; then
 	git stash
 	git pull --rebase
 	git stash apply
+	make clean
 	EXTRA_CXXFLAGS="-fpermissive" DEBUG_LEVEL=0 /usr/bin/time make static_lib db_bench -j $(nproc)
 	popd
 
@@ -12,5 +13,5 @@ if [ $UID != 0 ]; then
 	exit 0
 fi
 
-sudo RBENCH_DEV_NAME="nvme0n1" RBENCH_DEV_MODE="nvm"    ./run.sh 2>&1 | tee -a /tmp/rdb_nvme0n1_nvm.log
-sudo RBENCH_DEV_NAME="nvme1n1" RBENCH_DEV_MODE="legacy" ./run.sh 2>&1 | tee -a /tmp/rdb_nvme1n1_legacy.log
+sudo RBENCH_DEV_NAME="nvme0n1" RBENCH_DEV_MODE="nvm"    /usr/bin/time ./run.sh 2>&1 | tee /tmp/rdb_nvme0n1_nvm.log
+#sudo RBENCH_DEV_NAME="nvme1n1" RBENCH_DEV_MODE="legacy" /usr/bin/time ./run.sh 2>&1 | tee /tmp/rdb_nvme1n1_legacy.log
