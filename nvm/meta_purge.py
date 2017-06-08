20ead09467aff4c1b3abec0ba9669a202fa09434
Simon A. F. Lund <slund@cnexlabs.com>
2017-06-08 12:25:15 +0200

    Added meta_purge.py script

--- nvm/meta_purge.py
+++ nvm/meta_purge.py
@@ -4,6 +4,7 @@ from subprocess import Popen, PIPE
 import argparse
 import hashlib
 import shutil
+import copy
 import time
 import yaml
 import os
@@ -46,24 +47,38 @@ def main(args):
 
 	backup(mpath)
 
-	meta = [line.strip() for line in open(mpath).readlines()]
-	dev = "/dev/%s" % meta[0].strip()
+	orig = [line.strip() for line in open(mpath).readlines()]
+	dev = "/dev/%s" % orig[0].strip()
 
-	for blk, state in enumerate((int(m, 16) for m in meta[3:])):
+	meta = copy.deepcopy(orig)
+
+	print("dry: %s, mpath: %s" % (args.dry, mpath))
+
+	for blk, state in enumerate((int(m, 16) for m in orig[3:])):
 		if state not in [1, 4]:
 			continue
 
-		print("Erasing: {blk: %04d, dev: %s}" % (blk, dev))
+		print("Erasing: { blk: %04d, dev: %s }" % (blk, dev))
 		rcode, out, err = erase_cmd(dev, blk, args.dry)
-		print("Result: {\n  rcode: %d,\n  out: %s,\n  err: %s\n}" % (
-			rcode, out[:70], err[:70]
+
+		elapsed = []
+		for line in out.split("\n"):
+			if "elapsed" not in line.lower():
+				continue
+
+			elapsed.append(line)
+
+		print("Result: {\n  rcode: %d,\n  out: '%s',\n  err: '%s'\n}" % (
+			rcode,
+			" | ".join(elapsed),
+			" | ".join(err.split("\n"))[:110]
 		))
 
-		if not args.dry:
+		if args.dry:
 			continue
 
 		meta[blk+3] = "0x08" if rcode else "0x02"
-
+		print("Updating meta file")
 		with open(mpath, "w") as mfile:
 			for line in meta:
 				mfile.write("%s\n" % line)
